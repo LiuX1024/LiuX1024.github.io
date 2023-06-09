6b02dd0e69eb35a255645c1f422d8a5becef5ccf
LiuX1024 <130120785+LiuX1024@users.noreply.github.com>
2023-06-09 15:03:28 +0800

    Update InitCamera.js

--- js/InitCamera.js
+++ js/InitCamera.js
@@ -64,16 +64,16 @@ function onSuccess(position) {
 function onError(error) {
     switch (error.code) {
         case error.PERMISSION_DENIED:
-            alert("User denied the request for Geolocation.");
+            console.log("User denied the request for Geolocation.");
             break;
         case error.POSITION_UNAVAILABLE:
-            alert("Location information is unavailable.");
+            console.log("Location information is unavailable.");
             break;
         case error.TIMEOUT:
-            alert("The request to get user location timed out.");
+            console.log("The request to get user location timed out.");
             break;
         default:
-            alert("An unknown error occurred.");
+            console.log("An unknown error occurred.");
             break;
     }
 }
